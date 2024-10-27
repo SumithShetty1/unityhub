3f4879f71caee794dcbc480077e8d98cd8187d5e
yashaswinikunder <yashaswinikunder24@gmail.com>
2024-10-27 16:29:45 +0530

    Added Chatbox Frontend

---
+++ static/js/chat.js
@@ -0,0 +1,32 @@
+// Fetch the room name from the HTML data attribute
+const roomName = document.getElementById('room-name').dataset.roomName;
+
+// Initialize WebSocket
+const chatSocket = new WebSocket(
+    ws://${window.location.host}/ws/chat/${roomName}/
+);
+
+chatSocket.onmessage = function(e) {
+    const data = JSON.parse(e.data);
+    document.querySelector('#chat-log').innerHTML += <p>${data.message}</p>;
+};
+
+chatSocket.onclose = function(e) {
+    console.error('Chat socket closed unexpectedly');
+};
+
+document.querySelector('#chat-message-input').focus();
+document.querySelector('#chat-message-input').onkeyup = function(e) {
+    if (e.keyCode === 13) {  // Enter key
+        document.querySelector('#chat-message-submit').click();
+    }
+};
+
+document.querySelector('#chat-message-submit').onclick = function(e) {
+    const messageInputDom = document.querySelector('#chat-message-input');
+    const message = messageInputDom.value;
+    chatSocket.send(JSON.stringify({
+        'message': message
+    }));
+    messageInputDom.value = '';
+};
