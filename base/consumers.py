e84b018d992c92c2312bb6da930fcfc20ab32ffb
Santhoji V <santhoji07msd@gmail.com>
2024-11-01 20:12:39 +0530

    backends 404 (error)

--- base/consumers.py
+++ base/consumers.py
@@ -1,7 +1,6 @@
-# yourapp/consumers.py
-
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import ChatMessage  # Import your ChatMessage model
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -26,20 +25,32 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        user_name = text_data_json['user_name']  # Get the user's name from the message data
+
+        # Save the message to the database
+        await self.save_message(self.room_name, user_name, message)
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user_name': user_name  # Include the user's name in the message event
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
+        user_name = event['user_name']  # Retrieve the user's name from the event
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'user_name': user_name  # Include the user's name in the response
         }))
+
+    async def save_message(self, room_name, user_name, message):
+        # Create and save a new chat message instance
+        chat_message = ChatMessage(room_name=room_name, user_name=user_name, message=message)
+        await database_sync_to_async(chat_message.save)()  # Save the message asynchronously
